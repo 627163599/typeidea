4373c4bc7563ae706a67008348e4a61743f55d56
627163599 <627163599@qq.com>
2020-04-19 19:38:58 +0800

    更新

--- typeidea/settings/develop.py
+++ typeidea/settings/develop.py
@@ -1,9 +1,15 @@
-from .base import * #NOQA
+from .base import *  #NOQA
 
 DEBUG = True
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'typeidea',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306'
+       # 'ENGINE': 'django.db.backends.sqlite3',
+       # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3') ,
     }
 }
